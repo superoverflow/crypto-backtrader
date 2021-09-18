1d5e892e39f455f919d0ef0d95a14ee290672dd5
Cyrus Tang <cyrus.tang@superoverflow.com>
2021-09-18 20:29:54 +0100

    refactor

--- src/backtest.py
+++ src/backtest.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import backtrader as bt
 
-def loadBinanceData():
+
+def loadBinanceData(filename: str) -> pd.DataFrame:
     raw = pd.read_csv(
-        "data/BTCUSDT-1d.dat",
+        filename,
         header=None,
         usecols=range(0, 6),
         names=["datetime", "high", "low", "open", "close", "volume"]
@@ -11,6 +12,7 @@ def loadBinanceData():
     raw["datetime"] = raw["datetime"].apply(lambda x: pd.to_datetime(x/1000))
     raw = raw.set_index('datetime')
     return raw
+
 
 class SmaCross(bt.Strategy):
     params = dict(
@@ -34,16 +36,16 @@ class SmaCross(bt.Strategy):
 
 cerebro = bt.Cerebro()
 
-data = bt.feeds.PandasData(dataname=loadBinanceData())
+data = bt.feeds.PandasData(dataname=loadBinanceData("data/ADAUSDT-1h.dat"))
 
 cerebro.adddata(data)
 cerebro.addstrategy(SmaCross)
 
 start_portfolio_value = cerebro.broker.getvalue()
 cerebro.run()
+cerebro.plot()
 end_portfolio_value = cerebro.broker.getvalue()
 pnl = end_portfolio_value - start_portfolio_value
 print(f'Starting Portfolio Value: {start_portfolio_value:2f}')
 print(f'Final Portfolio Value: {end_portfolio_value:2f}')
 print(f'PnL: {pnl:.2f}')
-cerebro.plot()
